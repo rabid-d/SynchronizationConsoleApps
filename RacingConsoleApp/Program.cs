1097c21b3e6f63336bd2c4a189f98033925d0400
andrii <a.palamar777@gmail.com>
2022-07-22 16:28:41 +0300

    Add two tasks

---
+++ RacingConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Write("Enter number of racing threads: ");
+var n = int.Parse(Console.ReadLine());
+var rand = new Random();
+var roundTurns = new List<int>();
+var lines = new string[n];
+var tokenSource = new CancellationTokenSource();
+CancellationToken ct = tokenSource.Token;
+Console.Clear();
+
+foreach (var i in Enumerable.Range(0, n))
+{
+    Task.Run(() =>
+    {
+        while (true)
+        {
+            Monitor.Enter(typeof(Console));
+            if (ct.IsCancellationRequested)
+            {
+                break;
+            }
+            if (roundTurns.Contains(i))
+            {
+                if (roundTurns.Count == n)
+                {
+                    roundTurns.Clear();
+                }
+                else
+                {
+                    Monitor.Exit(typeof(Console));
+                    continue;
+                }
+            }
+            Console.SetCursorPosition(lines[i]?.Length ?? 0, i);
+            int randValue = rand.Next(1, 3);
+            for (int j = 0; j < randValue; j++)
+            {
+                Console.Write('-');
+                lines[i] += '-';
+            }
+            roundTurns.Add(i);
+            if (lines[i].Length >= 50)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.WriteLine($"Thread #{i + 1} is the winner!");
+                tokenSource.Cancel();
+            }
+            Thread.Sleep(10);
+            Monitor.Exit(typeof(Console));
+        }
+    }, tokenSource.Token);
+}
+
+Console.ReadKey();
---
+++ ReadAndWriteToFileConsoleApp/Program.cs
@@ -0,0 +1,88 @@
+﻿var rwlock = new ReaderWriterLockSlim();
+var fileName = "file.txt";
+
+Task.Run(() =>
+{
+    rwlock.EnterWriteLock();
+    Console.WriteLine($"Enter write lock: {GetDateTime()}");
+    using (var writetext = new StreamWriter(fileName))
+    {
+        for (int i = 0; i < 1500; i++)
+        {
+            string text = "";
+            for (int j = 0; j < 150; j++)
+            {
+                text += Guid.NewGuid();
+            }
+            writetext.WriteLine(GetDateTime() + " " + text);
+            Thread.Sleep(1);
+        }
+    }
+    Console.WriteLine($"Exit write lock: {GetDateTime()}\n");
+    rwlock.ExitWriteLock();
+});
+
+Thread.Sleep(500);
+
+foreach (var i in Enumerable.Range(1, 5))
+{
+    Task.Run(() =>
+    {
+        rwlock.EnterReadLock();
+        Console.WriteLine("Enter read lock: " + GetDateTime());
+        string text = File.ReadAllText(fileName);
+        File.WriteAllText($"file{i}.txt", text);
+        Console.WriteLine("Exit read lock: " + GetDateTime());
+        rwlock.ExitReadLock();
+    });
+}
+
+string GetDateTime() => DateTime.UtcNow.ToString("HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture);
+
+Console.ReadKey();
+
+
+/*  Semaphore version.  */
+
+var semaphore = new Semaphore(1, 5);
+var fileName = "file.txt";
+
+var writeTask = Task.Run(() =>
+{
+    semaphore.WaitOne();
+    Console.WriteLine($"Enter write lock: {GetDateTime()}");
+    using (var writetext = new StreamWriter(fileName))
+    {
+        for (int i = 0; i < 1500; i++)
+        {
+            string text = "";
+            for (int j = 0; j < 150; j++)
+            {
+                text += Guid.NewGuid();
+            }
+            writetext.WriteLine(GetDateTime() + " " + text);
+            Thread.Sleep(1);
+        }
+    }
+    Console.WriteLine($"Exit write lock: {GetDateTime()}\n");
+    semaphore.Release(5);
+});
+
+Thread.Sleep(500);
+
+foreach (var i in Enumerable.Range(1, 5))
+{
+    Task.Run(() =>
+    {
+        semaphore.WaitOne();
+        Console.WriteLine("Enter read lock: " + GetDateTime());
+        string text = File.ReadAllText(fileName);
+        File.WriteAllText($"file{i}.txt", text);
+        Console.WriteLine("Exit read lock: " + GetDateTime());
+        semaphore.Release();
+    });
+}
+
+string GetDateTime() => DateTime.UtcNow.ToString("HH:mm:ss.fff", System.Globalization.CultureInfo.InvariantCulture);
+
+Console.ReadKey();
